e7b26e5977b42c5e65572d3cb30509a89d995fab
Agooon <agonplp@gmail.com>
2021-12-01 00:10:15 +0100

    [Ćwiczenie 3] Dodanie kodu do raportu

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,11 +2,14 @@ import numpy as np
 import time
 import copy
 import random
+import math 
+  
 
 from numpy.core.fromnumeric import size
 
 class NeuralNetwork:
-    def __init__(self, sizes, epochs=100, learning_rate=0.1, mini_batch_size = 4, weights_scale = 0.1, weights_loc = 0, early_stop_value = 0.05):
+    def __init__(self, sizes, epochs=100, learning_rate=0.1, mini_batch_size = 4, weights_scale = 0.1, weights_loc = 0, early_stop_value = 0.05, 
+                momentum = 0.7, weight_scale_type ='normal', ad_gamma= 0.9, beta_1 =0.9, beta_2 = 0.999):
         # Tablica z ilością neuronów na poszczególnych wartstwach
         # Tutaj zastosowałem 4 warstwy, wejściową, 2 ukryte i wyjściową
         self.sizes = sizes
@@ -16,13 +19,25 @@ class NeuralNetwork:
         self.mini_batch_size = mini_batch_size
         
         # Parametry - przechowuje wagi oraz bias'y, oraz później poszczególne
-        self.params = self.initialize(weights_scale, weights_loc)
+
+        self.params = self.initialize(weights_scale, weights_loc, weight_scale_type)
 
         self.early_stop_value = early_stop_value
         self.best_error = 1
         self.best_params = copy.deepcopy(self.params)
 
-    def initialize(self, weights_scale = 0.1, weights_loc =0):
+
+        self.momentum = momentum
+
+        self.ad_gamma = ad_gamma
+
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.t = 1
+
+
+
+    def initialize(self, weights_scale = 0.1, weights_loc =0, weight_scale_type = 'normal'):
         # Przypisanie wielkości poszególnych warstw
         input_layer_size = self.sizes[0]
         hidden_layers_sizes = []
@@ -35,24 +50,118 @@ class NeuralNetwork:
         params = {}
 
         if(hidden_layers_sizes != []):
+            # Xavier
+            if weight_scale_type == 'xavier':
+                weights_scale = np.sqrt(2 / (hidden_layers_sizes[0] + input_layer_size))
+            # HE
+            elif weight_scale_type == 'he':
+                weights_scale = np.sqrt(2 / (input_layer_size))
+            
             params['W1'] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(hidden_layers_sizes[0], input_layer_size)) 
-            params['B1'] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(hidden_layers_sizes[0])) 
+            params['PW1'] = np.zeros( shape=(hidden_layers_sizes[0], input_layer_size)) 
+            params['GSW1'] = np.zeros( shape=(hidden_layers_sizes[0], input_layer_size)) 
+            params['MNW1'] = np.zeros( shape=(hidden_layers_sizes[0], input_layer_size)) 
+            params['AGAW1'] = np.zeros( shape=(hidden_layers_sizes[0], input_layer_size)) 
+            params['ADAW1'] = np.zeros( shape=(hidden_layers_sizes[0], input_layer_size))
+            params['MPW1'] = np.zeros( shape=(hidden_layers_sizes[0], input_layer_size)) 
+            params['VPW1'] = np.zeros( shape=(hidden_layers_sizes[0], input_layer_size)) 
+
+            params['B1'] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(hidden_layers_sizes[0]))
+            params['PB1'] = np.zeros( shape=(hidden_layers_sizes[0])) 
+            params['GSB1'] = np.zeros( shape=(hidden_layers_sizes[0])) 
+            params['MNB1'] = np.zeros( shape=(hidden_layers_sizes[0])) 
+            params['AGAB1'] = np.zeros( shape=(hidden_layers_sizes[0])) 
+            params['ADAB1'] = np.zeros( shape=(hidden_layers_sizes[0]))
+            params['MPB1'] = np.zeros( shape=(hidden_layers_sizes[0])) 
+            params['VPB1'] = np.zeros( shape=(hidden_layers_sizes[0]))  
         else:
-            params['W1'] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size, input_layer_size)) 
-            params['B1'] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size)) 
+            # Xavier
+            if weight_scale_type == 'xavier':
+                weights_scale = np.sqrt(2 / (output_layer_size + input_layer_size))
+            # He
+            elif weight_scale_type == 'he':
+                weights_scale = np.sqrt(2 / (input_layer_size))
+
+            params['W1'] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size, input_layer_size))
+            params['PW1'] = np.zeros( shape=(output_layer_size, input_layer_size)) 
+            params['GSW1'] = np.zeros( shape=(output_layer_size, input_layer_size)) 
+            params['MNW1'] = np.zeros( shape=(output_layer_size, input_layer_size)) 
+            params['AGAW1'] = np.zeros( shape=(output_layer_size, input_layer_size)) 
+            params['ADAW1'] = np.zeros( shape=(output_layer_size, input_layer_size)) 
+            params['MPW1'] = np.zeros( shape=(output_layer_size, input_layer_size)) 
+            params['VPW1'] = np.zeros( shape=(output_layer_size, input_layer_size)) 
+
+            params['B1'] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size))
+            params['PB1'] = np.zeros( shape=(output_layer_size)) 
+            params['GSB1'] = np.zeros( shape=(output_layer_size)) 
+            params['MNB1'] = np.zeros( shape=(output_layer_size)) 
+            params['AGAB1'] = np.zeros( shape=(output_layer_size)) 
+            params['ADAB1'] = np.zeros( shape=(output_layer_size))
+            params['MPB1'] = np.zeros( shape=(output_layer_size)) 
+            params['VPB1'] = np.zeros( shape=(output_layer_size))
 
         for i in range(len(hidden_layers_sizes)):
             if(i < len(hidden_layers_sizes)-1):
-                params['W'+str(i+2)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(hidden_layers_sizes[i+1], hidden_layers_sizes[i])) 
-                params['B'+str(i+2)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(hidden_layers_sizes[i+1])) 
-            else:
-                params['W'+str(i+2)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size, hidden_layers_sizes[i])) 
+                # Xavier
+                if weight_scale_type == 'xavier':
+                    weights_scale = np.sqrt(2 / (hidden_layers_sizes[i+1] + hidden_layers_sizes[i]))
+                # He
+                elif weight_scale_type == 'he':
+                    weights_scale = np.sqrt(2 / (hidden_layers_sizes[i]))
 
-        params['B'+str(len(hidden_layers_sizes)+1)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size)) 
+                params['W'+str(i+2)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(hidden_layers_sizes[i+1], hidden_layers_sizes[i]))
+                params['PW'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1], hidden_layers_sizes[i])) 
+                params['GSW'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1], hidden_layers_sizes[i])) 
+                params['MNW'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1], hidden_layers_sizes[i]))
+                params['AGAW'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1], hidden_layers_sizes[i])) 
+                params['ADAW'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1], hidden_layers_sizes[i]))
+                params['MPW'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1], hidden_layers_sizes[i])) 
+                params['VPW'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1], hidden_layers_sizes[i]))
+
+                params['B'+str(i+2)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(hidden_layers_sizes[i+1]))
+                params['PB'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1])) 
+                params['GSB'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1])) 
+                params['MNB'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1])) 
+                params['AGAB'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1])) 
+                params['ADAB'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1])) 
+                params['MPB'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1])) 
+                params['VPB'+str(i+2)] = np.zeros( shape=(hidden_layers_sizes[i+1])) 
+            else:
+                # Xavier
+                if weight_scale_type == 'xavier':
+                    weights_scale = np.sqrt(2 / (output_layer_size + hidden_layers_sizes[i]))
+                # He
+                elif weight_scale_type == 'he':
+                    weights_scale = np.sqrt(2 / (hidden_layers_sizes[i]))
+
+                params['W'+str(i+2)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size, hidden_layers_sizes[i]))
+                params['PW'+str(i+2)] = np.zeros( shape=(output_layer_size, hidden_layers_sizes[i])) 
+                params['GSW'+str(i+2)] = np.zeros( shape=(output_layer_size, hidden_layers_sizes[i])) 
+                params['MNW'+str(i+2)] = np.zeros( shape=(output_layer_size, hidden_layers_sizes[i]))
+                params['AGAW'+str(i+2)] = np.zeros( shape=(output_layer_size, hidden_layers_sizes[i])) 
+                params['ADAW'+str(i+2)] = np.zeros( shape=(output_layer_size, hidden_layers_sizes[i])) 
+                params['MPW'+str(i+2)] = np.zeros( shape=(output_layer_size, hidden_layers_sizes[i])) 
+                params['VPW'+str(i+2)] = np.zeros( shape=(output_layer_size, hidden_layers_sizes[i])) 
+
+        # Xavier
+        if weight_scale_type == 'xavier':
+            weights_scale = np.sqrt(2 / (output_layer_size))
+        # He
+        elif weight_scale_type == 'he':
+            weights_scale = np.sqrt(2 / (output_layer_size))
+
+        params['B'+str(len(hidden_layers_sizes)+1)] = np.random.normal(loc = weights_loc, scale=weights_scale, size=(output_layer_size))
+        params['PB'+str(len(hidden_layers_sizes)+1)] = np.zeros( shape=(output_layer_size)) 
+        params['GSB'+str(len(hidden_layers_sizes)+1)] = np.zeros( shape=(output_layer_size))
+        params['MNB'+str(len(hidden_layers_sizes)+1)] = np.zeros( shape=(output_layer_size))
+        params['AGAB'+str(len(hidden_layers_sizes)+1)] = np.zeros( shape=(output_layer_size))
+        params['ADAB'+str(len(hidden_layers_sizes)+1)] = np.zeros( shape=(output_layer_size))
+        params['MPB'+str(len(hidden_layers_sizes)+1)] = np.zeros( shape=(output_layer_size))
+        params['VPB'+str(len(hidden_layers_sizes)+1)] = np.zeros( shape=(output_layer_size))
 
         return params
 
-    def forward(self, x_train, bias_active=True, sigmoid_active=True, tanhip_active=True):
+    def forward(self, x_train, bias_active=True, sigmoid_active=True, tanhip_active=True, momentum_nesterov = False):
         params = self.params
 
         cl = 0
@@ -64,10 +173,18 @@ class NeuralNetwork:
             cl += 1
             cls = str(cl)
             pls = str(cl-1)
-            if(bias_active):
-                params['Z'+cls] = np.add(np.dot(params["W" + cls], params['A'+pls]), params['B'+cls])
+
+            if not momentum_nesterov:
+                weight = params['W' + cls]
+                weight_b = params['B' + cls]
             else:
-                params['Z'+cls] = np.dot(params["W"+cls], params['A'+pls])
+                weight = params['MNW' + cls]
+                weight_b = params['MNB' + cls]
+
+            if(bias_active):
+                params['Z'+cls] = np.add(np.dot(weight, params['A'+pls]), weight_b)
+            else:
+                params['Z'+cls] = np.dot(weight, params['A'+pls])
 
             if(sigmoid_active):
                 params['A'+cls] = self.sigmoid(params['Z'+cls])
@@ -79,16 +196,24 @@ class NeuralNetwork:
         cl += 1
         cls = str(cl)
         pls = str(cl-1)
-        if(bias_active):
-            params['Z'+cls] = np.add(np.dot(params["W"+cls], params['A'+pls]), params['B'+cls])
+
+        if not momentum_nesterov:
+            weight = params['W' + cls]
+            weight_b = params['B' + cls]
         else:
-            params['Z'+cls] = np.dot(params["W"+cls], params['A'+pls])
+            weight = params['MNW' + cls]
+            weight_b = params['MNB' + cls]
+
+        if(bias_active):
+            params['Z'+cls] = np.add(np.dot(weight, params['A'+pls]), weight_b)
+        else:
+            params['Z'+cls] = np.dot(weight, params['A'+pls]) 
 
         params['A'+cls] = self.softmax(params['Z'+cls])
 
         return params['A'+cls]
 
-    def backward(self, y_train, output, sigmoid_active=True, tanhip_active=True):
+    def backward(self, y_train, output, sigmoid_active=True, tanhip_active=True, momentum_nesterov = False):
         params = self.params
         change_weights = {}
         cl = self.layer_count - 1
@@ -110,21 +235,59 @@ class NeuralNetwork:
             pls = str(cl-1)
             nls = str(cl+1)
 
-            if(sigmoid_active):
-                error = np.dot(params['W' + nls].T, error) * self.sigmoid(params['Z' + cls], derivative=True)
-            elif(tanhip_active):
-                error = np.dot(params['W' + nls].T, error) * self.tanhip(params['Z' + cls], derivative=True)
+            if not momentum_nesterov:
+                weight_t = params['W' + nls].T
             else:
-                error = np.dot(params['W' + nls].T, error) * self.ReLU(params['Z' + cls], derivative=True)
+                weight_t = params['MNW' + nls].T
+            
+
+            if(sigmoid_active):
+                error = np.dot(weight_t, error) * self.sigmoid(params['Z' + cls], derivative=True)
+            elif(tanhip_active):
+                error = np.dot(weight_t, error) * self.tanhip(params['Z' + cls], derivative=True)
+            else:
+                error = np.dot(weight_t, error) * self.ReLU(params['Z' + cls], derivative=True)
 
             change_weights['W' + cls] = np.outer(error, params['A' + pls])
             change_weights['B'+ cls] = error
 
         return change_weights
 
-    def update_network_parameters(self, changes_to_w):
+    def update_network_parameters(self, changes_to_w, learning_rate_mode = 'normal', epsilon=1e-8):
         for key, value in changes_to_w.items():
-            self.params[key] -= self.learning_rate * value
+            if learning_rate_mode == 'momentum':
+                self.params['P' + key] = self.learning_rate * value + self.momentum * self.params['P' + key]
+                self.params[key] -= self.params['P' + key]
+
+            elif learning_rate_mode == 'momentum_nesterov':
+                self.params['P' + key] = self.learning_rate * value + self.momentum * self.params['P' + key]
+                self.params[key] -= self.params['P' + key]
+                self.params['MN' + key] = self.params[key] - self.momentum * self.params['P' + key]
+
+            elif learning_rate_mode == 'adagrad':
+                self.params['GS' + key] += (value) ** 2
+                self.params[key] -= (self.learning_rate * value) / (np.sqrt(self.params['GS' + key] + epsilon))
+
+            elif learning_rate_mode == 'adelta':
+                self.params['AGA' + key] = (self.ad_gamma * self.params['AGA' + key]) + (1 - self.ad_gamma) * np.square(value)
+                new_value = (np.sqrt(self.params['ADA' + key] + epsilon)) / (np.sqrt(self.params['AGA' + key] + epsilon)) * value
+                self.params['ADA' + key] = (self.ad_gamma * self.params['ADA' + key]) + (1 - self.ad_gamma) * np.square(new_value)
+                self.params[key] -= new_value
+
+            elif learning_rate_mode == 'adam':
+                self.params['MP' + key] = self.beta_1 * self.params['MP' + key] + (1-self.beta_1) * value 
+                self.params['VP' + key] = self.beta_2 * self.params['VP' + key] + (1-self.beta_2) * np.square(value)
+
+                m_corrected = self.params['MP' + key] / (1-self.beta_1**self.t)
+                v_corrected = self.params['VP' + key] / (1-self.beta_2**self.t)
+
+                self.params[key] -= self.learning_rate * m_corrected / (np.sqrt(v_corrected) + 1e-8) 
+            else:
+                # default: normal
+                self.params[key] -= self.learning_rate * value
+
+        self.t+=1
+
 
 
     def compute_accuracy(self, x_val, y_val):
@@ -137,7 +300,7 @@ class NeuralNetwork:
 
         return predictions_sum/len(y_val)
 
-    def train_with_batch(self, x_train, y_train, x_val, y_val, bias_active = True, activation_function = 'sig'):
+    def train_with_batch(self, x_train, y_train, x_val, y_val, bias_active = True, activation_function = 'sig', learning_rate_mode = 'normal', momentum_nesterov = False):
         if(activation_function == 'sig'):
             sigmoid_active = True
             tanhip_active = False
@@ -148,6 +311,11 @@ class NeuralNetwork:
             sigmoid_active = False
             tanhip_active = False
 
+        if learning_rate_mode == 'momentum_nesterov':
+            momentum_nesterov = True
+        else:
+            momentum_nesterov = False
+
         training_size = x_train.shape[0]
         start_time = time.time() 
         for iteration in range(self.epochs):
@@ -156,17 +324,17 @@ class NeuralNetwork:
             x_train, y_train = zip(*c)
 
             for i in range(0, training_size, self.mini_batch_size):
-                output = self.forward(x_train[i], bias_active, sigmoid_active, tanhip_active)
-                change = self.backward(y_train[i], output, sigmoid_active, tanhip_active)
+                output = self.forward(x_train[i], bias_active, sigmoid_active, tanhip_active, momentum_nesterov)
+                change = self.backward(y_train[i], output, sigmoid_active, tanhip_active, momentum_nesterov)
    
                 for x, y in zip(x_train[i+1 : min(i+self.mini_batch_size, training_size -1)], y_train[i+1 : min(i+self.mini_batch_size, training_size -1)]):
-                    output = self.forward(x, bias_active, sigmoid_active, tanhip_active)
-                    next_change = self.backward(y, output, sigmoid_active, tanhip_active)
+                    output = self.forward(x, bias_active, sigmoid_active, tanhip_active, momentum_nesterov)
+                    next_change = self.backward(y, output, sigmoid_active, tanhip_active, momentum_nesterov)
                     change = {key: np.add(change[key], next_change[key])  for key in change.keys()}
 
                 change = {key: change[key]/self.mini_batch_size for key in change.keys()}
 
-                self.update_network_parameters(change)
+                self.update_network_parameters(change, learning_rate_mode)
             
             accuracy = self.compute_accuracy(x_val, y_val)
             # print('[Training_size: {5}][Batch: {4}][{3}] Epoka: {0}, Czas: {1:.2f}s, Procent dokłądności: {2:.2f}%'.format(
